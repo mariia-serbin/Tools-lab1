2f6ab040cb7baee8811dcaa77af204920542c9f0
mariia-serbin <mariia-serbin@users.noreply.github.com>
2025-11-23 12:21:54 +0000

    deploy: acd915c268c01f4c690115ccda31d252d6d6fb5b

--- classmatrix_1_1Matrix.js
+++ classmatrix_1_1Matrix.js
@@ -1,4 +1,5 @@
 var classmatrix_1_1Matrix =
 [
-    [ "add", "classmatrix_1_1Matrix.html#a9db7fa85cbf8c22523e1bdd108be1797", null ]
+    [ "add", "classmatrix_1_1Matrix.html#a9db7fa85cbf8c22523e1bdd108be1797", null ],
+    [ "subtract", "classmatrix_1_1Matrix.html#a3ef4162d8fae8e83657ba781218485c0", null ]
 ];
--- search/all_7.js
+++ search/all_7.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['setup_0',['setUp',['../classmatrix__tests_1_1MatrixTests.html#ab1be8078a320d3737d00621e011fa8f6',1,'matrix_tests::MatrixTests']]]
+  ['setup_0',['setUp',['../classmatrix__tests_1_1MatrixTests.html#ab1be8078a320d3737d00621e011fa8f6',1,'matrix_tests::MatrixTests']]],
+  ['subtract_1',['subtract',['../classmatrix_1_1Matrix.html#a3ef4162d8fae8e83657ba781218485c0',1,'matrix::Matrix']]]
 ];
